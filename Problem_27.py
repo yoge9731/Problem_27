79a6eaf3203ccf63ffa6816b9acdad1135ed4aa5
YOGEESH K P <yogeesh793@gmail.com>
2025-01-17 23:10:18 +0530

    Add files via upload

---
+++ Problem_27.py
@@ -0,0 +1,24 @@
+"""List Manipulation:
+
+Create a list of Kannada foods. Use list comprehension to create a new list where each food name is in uppercase."""
+
+foods = ["idly","dose","poori"]
+u_foods = [item.upper() for item in foods]
+print(u_foods)
+
+#2
+foods = ["upma","dose","ricebath"]
+u_foods = [i.upper() for i in foods]
+print(u_foods)
+
+#3
+foods = ["friderice","gerarice","menthayarice"]
+u_foods = [i.upper() for i in foods]
+print(u_foods)
+
+#4
+foods = ["lemonrice","rice & samber","tomotorice"]
+u_foods = [i.upper() for i in foods]
+print(u_foods)
+
+
